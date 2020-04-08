3ca96ade1ca253c04fd57b807972a0f72879d877
Steven Batcup <steve.batcup@gmail.com>
2020-04-08 14:40:17 +0100

    fixed password reset

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,4 +1,16 @@
-//= require ./controllers/includes/angular_base
-//= require ./controllers/includes/page
-//= require ./controllers/helpers/form_helpers
-//= require ./controllers/admin
+//= require jquery
+//= require rails-ujs
+//= require bootstrap
+//= require underscore
+
+//= require angular
+//= require angular-animate
+//= require angularjs/rails/resource
+//= require angularjs/rails/resource/extensions/snapshots
+
+//= require angular_bootstrap
+
+//= require controllers/includes/angular_base
+//= require controllers/includes/page
+//= require controllers/helpers/form_helpers
+//= require controllers/admin
