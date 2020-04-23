edf592c2a2770681ad1d67a25454db3a8a6ab974
Steven Batcup <steve.batcup@gmail.com>
2020-04-23 10:01:54 +0100

    exception notifications

---
+++ lib/exception_notifier/mandrill_notifier.rb
@@ -0,0 +1,20 @@
+module ExceptionNotifier
+  class MandrillNotifier
+	  include Maawol::Email::Mandrill
+
+    def initialize(options)
+      @subject = options[:subject]
+      @recipient_address = options[:recipient_address]
+    end
+
+    def call(exception, options={})
+    	merge_vars = {
+    		EXCEPTION_MSG: exception.message,
+    		EXCEPTION_BACKTRACE: exception.backtrace.inspect
+    	}
+			body = template('exception', merge_vars)
+  	  data = mail_data(@recipient_address, @subject, body)
+		  response = api.messages.send(data)
+    end
+  end
+end
