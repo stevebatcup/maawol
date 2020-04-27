b404d959f5153416179ed4c55108409f5fc98580
Steven Batcup <steve.batcup@gmail.com>
2020-04-27 13:20:00 +0100

    bugs with contact forms

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -2,13 +2,16 @@ class ContactController < MaawolController
 	def new
 		@contact = Contact.new
 
-		@lesson_request = params[:lesson_request].present?
-		@selected_subject = @lesson_request ? "Request a lesson" : "Other enquiry"
-		require_subscription if @lesson_request
-
-		if @stuck = params[:stuck].present?
-			@contact.subject = "Help! I don't know what to work on!"
-			@questions = StuckQuestion.includes(:stuck_answers).references(:stuck_answers).order(sort: :asc)
+		if signed_in?
+			if params[:lesson_request].present?
+				require_subscription
+				@contact.subject = "Request a lesson"
+				@lesson_request = true
+			elsif params[:stuck].present?
+				setup_stuck_form
+			else
+				@contact.subject = "Other enquiry"
+			end
 		end
 	end
 
@@ -18,35 +21,46 @@ class ContactController < MaawolController
 			email: contact_params[:email],
 			subject: contact_params[:subject]
 		})
-		@contact.user_id = current_user.id if user_signed_in?
-		if contact_params[:message].present?
-			@contact[:message] = contact_params[:message]
-		else
+		@contact.user_id = current_user.id if signed_in?
+		if is_stuck_submission?
 			@contact.gather_question_data(contact_params)
+		else
+			@contact[:message] = contact_params[:message]
 		end
-		if !user_signed_in? && Rails.env.production? && !verify_recaptcha(model: @contact, secret_key: Rails.application.credentials.recaptcha[:secret_key])
+		if !signed_in? && Rails.env.production? && !verify_recaptcha(model: @contact, secret_key: Rails.application.credentials.recaptcha[:secret_key])
 			flash[:alert] = @contact.errors.full_messages[0]
 			render :new
 		else
 			if @contact.save
 				if contact_params[:message].present?
-					notice = "Thanks I'll get back in touch ASAP"
+					notice = "Thanks We'll be back in touch ASAP"
 					if contact_params[:from_home].present?
 						redirect_to root_path, notice: notice
 					else
 						redirect_to new_contact_path, notice: notice
 					end
 				else
-					redirect_to lessons_path(stuck: true), notice: "Thanks! I'll get back to you ASAP with some suggestions of things to work on."
+					redirect_to lessons_path(stuck: true), notice: "Thanks! We'll get back to you ASAP with some suggestions of things to work on."
 				end
 			else
 				flash[:alert] = "Sorry there was an error sending your message: #{legible_form_errors(@contact.errors)}"
+				setup_stuck_form if is_stuck_submission?
 				render :new
 			end
 		end
 	end
 
 private
+
+	def setup_stuck_form
+		@stuck = true
+		@contact.subject = "Help! I don't know what to work on!"
+		@questions = StuckQuestion.includes(:stuck_answers).references(:stuck_answers).order(sort: :asc)
+	end
+
+	def is_stuck_submission?
+		!contact_params[:message].present?
+	end
 
 	def contact_params
 		params.require(:contact).permit!
