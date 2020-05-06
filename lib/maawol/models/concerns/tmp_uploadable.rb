7bcded60f7dc0031ed60556f6bba875cd28cef39
Steven Batcup <steve.batcup@gmail.com>
2020-05-06 12:32:10 +0100

    bug fix

--- lib/maawol/models/concerns/tmp_uploadable.rb
+++ lib/maawol/models/concerns/tmp_uploadable.rb
@@ -5,7 +5,7 @@ module Maawol
 				extend ActiveSupport::Concern
 
 			  def migrate_file_from_tmp_upload
-			  	self.send("#{self.field_for_upload}=", uploaded_file_uri)
+			  	self.send("#{self.field_for_upload}=", File.new(uploaded_file_uri))
 			  	self.tmp_media_id = nil
 			  	self.save!
 			  	associated_tmp_medium.destroy
@@ -13,7 +13,7 @@ module Maawol
 
 			  def uploaded_file_uri
 			  	if Rails.env.development?
-				  	File.new(File.join(Rails.root, '/public' + associated_tmp_medium.media_file.url))
+				  	File.join(Rails.root, '/public' + associated_tmp_medium.media_file.url)
 				  else
 				  	associated_tmp_medium.media_file.url
 				  end
