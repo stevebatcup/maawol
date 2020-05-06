10452996aad7a8e1d9211c805360e1dbac3ce0ac
Steven Batcup <steve.batcup@gmail.com>
2020-05-06 11:54:32 +0100

    bug fix with production uploads

--- lib/maawol/models/concerns/tmp_uploadable.rb
+++ lib/maawol/models/concerns/tmp_uploadable.rb
@@ -5,10 +5,18 @@ module Maawol
 				extend ActiveSupport::Concern
 
 			  def migrate_file_from_tmp_upload
-			  	self.send("#{self.field_for_upload}=",  File.new(File.join(Rails.root, '/public' + associated_tmp_medium.media_file.url)))
+			  	self.send("#{self.field_for_upload}=", uploaded_file_uri
 			  	self.tmp_media_id = nil
 			  	self.save!
 			  	associated_tmp_medium.destroy
+			  end
+
+			  def uploaded_file_uri
+			  	if Rails.env.development?
+				  	File.new(File.join(Rails.root, '/public' + associated_tmp_medium.media_file.url))
+				  else
+				  	associated_tmp_medium.media_file.url
+				  end
 			  end
 
 			  def associated_tmp_medium
